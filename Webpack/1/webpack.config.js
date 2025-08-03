f536a8ea36c5db5ccadf77e1cf6e5abef7c45479
gcisadd <2374113375@qq.com>
2025-08-03 20:47:55 +0800

    Webpack

---
+++ Webpack/1/webpack.config.js
@@ -0,0 +1,6 @@
+// import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+// import CssMinimizerPlugin from'css-minimizer-webpack-plugin'
+// import HtmlWebpackPlugin from'html-webpack-plugin'
+export default {
+  mode: 'development',
+};
